5740232d76a33f766f9b912252fdf24265125a89
zinaida21 <you@example.com>
2021-11-17 07:58:28 +0300

    hm1

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 "use strict";
 const product = 'sofa';
-let count = 1000;
-let info = product + ', ' + 'цена ' + count + ' рублей';
+const count = 1000;
+const info = product + ', ' + 'цена ' + count + ' рублей';
 alert(info);
