2d2a12c87272773bd063c86be67bcc4e99cf0abe
zinaida21 <zinaida21@users.noreply.github.com>
2021-11-20 16:47:31 +0000

    Deploying to gh-pages from @ zinaida21/skypro@7b5aee527ac0538e1db407f5f4946a51fa1cb9b5 🚀

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 'use strict';
-const product = 'sofa';
+/*const product = 'sofa';
 const count = 1000;
 const info = product + ', ' + 'цена ' + count + ' рублей';
-alert(info);
+alert(info);*/
+
+let getRandomFromRange = (min, max) => {
+  if (min >= max) {
+    return console.log ('Error, min не может быть больше или равно max')
+  } else {
+    return Math.random() * (max - min) + min;
+  }
+}
+// eslint-disable-next-line
+console.log (parseInt(getRandomFromRange(1, 100)));
+
+
